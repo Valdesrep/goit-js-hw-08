dacf3a1b4a46f7594643f2c083e054a984d68ecc
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-02-18 15:45:28 +0200

    8 дз таск 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const saveTimeVideo = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = localStorage.getItem(saveTimeVideo)
+
+if (currentTime){
+    player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle((event) => {
+    localStorage.setItem(saveTimeVideo, event.seconds)    
+}, 1000) );
